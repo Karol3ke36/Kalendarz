73a9ce8f8007ab293648237c92a70cedce4bc0c6
Karol3ke36 <karolpucyk9@gmail.com>
2026-04-17 04:07:02 +0200

    zrobione

---
+++ app/src/main/java/com/example/kalendarz/KalendarzAdapter.java
@@ -0,0 +1,57 @@
+package com.example.kalendarz;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class KalendarzAdapter extends RecyclerView.Adapter<KalendarzAdapter.ViewHolder> {
+
+    private ArrayList<LocalDate> dni;
+
+    public KalendarzAdapter(ArrayList<LocalDate> dni) {
+        this.dni = dni;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.komorka_dnia, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        LocalDate date = dni.get(position);
+
+
+        if (date == null) {
+            holder.komorkaDnia.setText("");
+        }
+        else {
+            holder.komorkaDnia.setText(String.valueOf(date.getDayOfMonth()));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return dni.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView komorkaDnia;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            komorkaDnia = itemView.findViewById(R.id.komorkaDnia);
+        }
+    }
+}
--- app/src/main/java/com/example/kalendarz/MainActivity.java
+++ app/src/main/java/com/example/kalendarz/MainActivity.java
@@ -1,24 +1,80 @@
 package com.example.kalendarz;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private RecyclerView listaDni;
+    private Button buttonPrev, buttonNext;
+
+    private LocalDate selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
+    }
+
+    private void initWidgets() {
+        textView = findViewById(R.id.textView);
+        listaDni = findViewById(R.id.listaDni);
+        buttonPrev = findViewById(R.id.button);
+        buttonNext = findViewById(R.id.button2);
+
+        buttonPrev.setOnClickListener(v -> {
+            selectedDate = selectedDate.minusMonths(1);
+            setMonthView();
         });
+
+        buttonNext.setOnClickListener(v -> {
+            selectedDate = selectedDate.plusMonths(1);
+            setMonthView();
+        });
+    }
+
+    private void setMonthView() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("LLLL yyyy", new Locale("pl", "PL"));
+        textView.setText(selectedDate.format(formatter));
+
+        ArrayList<LocalDate> dni = daysInMonthArray(selectedDate);
+
+        KalendarzAdapter adapter = new KalendarzAdapter(dni);
+        listaDni.setLayoutManager(new GridLayoutManager(this, 7));
+        listaDni.setAdapter(adapter);
+    }
+
+    private ArrayList<LocalDate> daysInMonthArray(LocalDate date) {
+        ArrayList<LocalDate> days = new ArrayList<>();
+
+        YearMonth yearMonth = YearMonth.from(date);
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = date.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+        for (int i = 1; i <= 42; i++) {
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+                days.add(null);
+            } else {
+                days.add(LocalDate.of(date.getYear(), date.getMonth(), i - dayOfWeek));
+            }
+        }
+        return days;
     }
 }
